c3b5e8b8d569a7edb9ab1c4076207b1ab51a6c93
zsefvcx <zsefvcx@list.ru>
2023-07-25 16:44:12 +0300

    Lesson 18 Camera App Final

--- app_camera/lib/main.dart
+++ app_camera/lib/main.dart
@@ -1,13 +1,20 @@
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-late List<CameraDescription> _cameras;
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:camera/camera.dart';
+
+List<CameraDescription> _cameras = [];
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  _cameras = await availableCameras();
-  runApp(const CameraApp());
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    _cameras = await availableCameras();
+  } on CameraException catch (e) {
+    _logError(e.code, e.description);
+  }
+  runApp(const MaterialApp(home: CameraApp()));
 }
 
 /// CameraApp is the Main Application.
@@ -19,73 +26,127 @@ class CameraApp extends StatefulWidget {
   State<CameraApp> createState() => _CameraAppState();
 }
 
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
+class _CameraAppState extends State<CameraApp> with WidgetsBindingObserver{
+  CameraController? controller;
+  bool _isCameraInitialized = false;
+
 
   int _selectedIndex = 0;
 
+  List<XFile?> listImageFile = [];
+
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    final previousCameraController = controller;
+    // Instantiating the camera controller
+    final CameraController cameraController = CameraController(
+      cameraDescription,
+      ResolutionPreset.high,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+
+    // Dispose the previous controller
+    await previousCameraController?.dispose();
+
+    // Replace with the new controller
+    if (mounted) {
+      setState(() {
+        controller = cameraController;
+      });
+    }
+
+    // Update UI if controller updated
+    cameraController.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    // Initialize controller
+    try {
+      await cameraController.initialize();
+    } on CameraException catch (e) {
+      _logError(e.code, e.description);
+      showInSnackBar('Error initializing camera: ${e.code}\n${e.description}');
+    }
+
+    // Update the Boolean
+    if (mounted) {
+      setState(() {
+        _isCameraInitialized = controller!.value.isInitialized;
+      });
+    }
+  }
 
   @override
   void initState() {
+    onNewCameraSelected(_cameras[0]);
     super.initState();
-    controller = CameraController(_cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-          // Handle access errors here.
-            break;
-          default:
-          // Handle other errors here.
-            break;
-        }
-      }
-    });
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    controller?.dispose();
     super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    final CameraController? cameraController = controller;
 
-    if (!controller.value.isInitialized) {
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      return;
+    }
+
+    if (state == AppLifecycleState.inactive) {
+      // Free up memory when camera not active
+      cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      // Reinitialize the camera with same properties
+      onNewCameraSelected(cameraController.description);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final CameraController? cameraController = controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
       return Container();
     }
-    return MaterialApp(
-      home: Scaffold(
+    final scale = 1 / (cameraController.value.aspectRatio /** MediaQuery.of(context).size.aspectRatio*/);
+
+    return Scaffold(
         appBar: AppBar(centerTitle: true, title:
           _selectedIndex==0?const Text('Camera preview'):const Text('Images gallery')),
-
-
-          body: _selectedIndex==0?CameraPreview(controller):
+          body: _selectedIndex==0?SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: cameraController.buildPreview()//CameraPreview(cameraController),
+          ):
         ListView.builder(
-          itemCount: 10,
+          itemCount: listImageFile.length,
           itemBuilder: (context, index) {
-              return const Card(
+            XFile? imageFile = listImageFile[index];
+              return Card(
                   elevation: 0,
-                  margin: EdgeInsets.all(0),
+                  margin: const EdgeInsets.all(0),
                   child: SizedBox(
-                    height: 200,
-                    child: Placeholder(),
-                  ),
+                    height: 300, width: double.infinity,
+                    child: imageFile==null?const Placeholder(): kIsWeb
+                        ? Image.network(imageFile.path, fit: BoxFit.cover,)
+                        : Image.file(File(imageFile.path),fit: BoxFit.cover,)),
               );
             },
           ),
           floatingActionButton: Visibility(
             visible: _selectedIndex==0,
             child: FloatingActionButton(
-              onPressed: () {
-
-              },
+              onPressed: ()=> takePicture().then((XFile? file) {
+                if (file != null) {
+                  if(listImageFile.length>=10)listImageFile.removeLast();
+                  listImageFile.add(file);
+                  showInSnackBar('Picture saved to ${file.path}');
+                }
+              }),
               child: const Icon(Icons.camera),
             ),
           ),
@@ -97,7 +158,7 @@ class _CameraAppState extends State<CameraApp> {
               ),
               BottomNavigationBarItem(
                 icon: Icon(Icons.image_rounded),
-                label: 'Gellary',
+                label: 'Gallery',
               ),
             ],
             currentIndex: _selectedIndex,
@@ -106,9 +167,49 @@ class _CameraAppState extends State<CameraApp> {
               setState(() {
                 _selectedIndex = value;
               });
+              if(_selectedIndex == 0 && kIsWeb){
+                cameraController.dispose();
+                onNewCameraSelected(cameraController.description);
+              }
+
             },
           ),
-      ),
     );
   }
+
+  Future<XFile?> takePicture() async {
+    final CameraController? cameraController = controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      showInSnackBar('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      final XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    _logError(e.code, e.description);
+    showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  void showInSnackBar(String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
+}
+
+void _logError(String code, String? message) {
+  // ignore: avoid_print
+  print('Error: $code${message == null ? '' : '\nError Message: $message'}');
 }

f22303210cb3716a31e9043de3819f3f24af1814
zsefvcx <zsefvcx@list.ru>
2023-07-25 22:19:31 +0300

    lesson18 app video player 40%

--- app_video_player/lib/main.dart
+++ app_video_player/lib/main.dart
@@ -5,17 +5,14 @@ void main() => runApp(const VideoApp());
 
 /// Stateful widget to fetch and then display video content.
 class VideoApp extends StatefulWidget {
-  const VideoApp({super.key});
+  const VideoApp({super.key });
 
   @override
-  _VideoAppState createState() => _VideoAppState();
+  VideoAppState createState() => VideoAppState();
 }
 
-class _VideoAppState extends State<VideoApp>  with SingleTickerProviderStateMixin {
+class VideoAppState extends State<VideoApp>{
   late VideoPlayerController _controller;
-
-  late AnimationController _animationController;
-  late Animation _myAnimation;
 
   @override
   void initState() {
@@ -26,18 +23,6 @@ class _VideoAppState extends State<VideoApp>  with SingleTickerProviderStateMixi
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
-    _animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1200),
-
-    );
-    _myAnimation = CurvedAnimation(parent: _animationController, curve: Curves.easeIn);
-  }
-
-  @override
-  void didUpdateWidget(VideoApp oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _controller.duration = widget.duration;
   }
 
   @override
@@ -45,67 +30,88 @@ class _VideoAppState extends State<VideoApp>  with SingleTickerProviderStateMixi
     return MaterialApp(
       title: 'Video Demo',
       home: Scaffold(
-        body: Stack(
-          children: [
-            Center(
-              child: _controller.value.isInitialized
-                  ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-                  : Container(),
+        body: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                _controller.value.isPlaying
+                    ? _controller.pause()
+                    : _controller.play();
+              });
+            },
+            child: Stack(
+              children: [
+                Center(
+                  child: _controller.value.isInitialized
+                      ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  )
+                      : Container(),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: VideoProgressIndicator(_controller, allowScrubbing: true),
+                )
+              ],
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: VideoProgressIndicator(_controller, allowScrubbing: true),
-            )
-          ],
+          ),
         ),
-        floatingActionButton: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  setState(() async {
-                    var position = await _controller.position;
-                    if (position == null) return;
-                    await _controller.seekTo(Duration(seconds: position.inSeconds>10?position.inSeconds-10:0));
-                  });
-                },
-                backgroundColor: Colors.transparent,
-                child: const Icon(
-                  Icons.replay_10,
+        floatingActionButton: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 50),
+          reverseDuration: const Duration(milliseconds: 200),
+          child: _controller.value.isPlaying
+              ? const SizedBox.shrink()
+              : Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() async {
+                      var position = await _controller.position;
+                      if (position == null) return;
+                      await _controller.seekTo(Duration(seconds: position.inSeconds>10?position.inSeconds-10:0));
+
+                    });
+                  },
+                  backgroundColor: Colors.transparent,
+                  child: const Icon(
+                    Icons.replay_10,
+                  ),
                 ),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  setState(() {
-                    _controller.value.isPlaying
-                        ? _controller.pause()
-                        : _controller.play();
-                  });
-                },
-                backgroundColor: Colors.transparent,
-                child: Icon(
-                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _controller.value.isPlaying
+                          ? _controller.pause()
+                          : _controller.play();
+
+                    });
+                  },
+                  backgroundColor: Colors.transparent,
+                  child: Icon(
+                    _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                  ),
                 ),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  setState(() async {
-                    var position = await _controller.position;
-                    if (position == null) return;
-                    await _controller.seekTo(Duration(seconds: position.inSeconds+10));
-                  });
-                },
-                backgroundColor: Colors.transparent,
-                child: const Icon(
-                  Icons.forward_10,
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() async {
+                      var position = await _controller.position;
+                      if (position == null) return;
+                      await _controller.seekTo(Duration(seconds: position.inSeconds+10));
+
+                    });
+                  },
+                  backgroundColor: Colors.transparent,
+                  child: const Icon(
+                    Icons.forward_10,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
